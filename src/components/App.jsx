bce08b0e32a028b465c20110831c0fd5e00df4ca
Reese Lee and Stuart McKay <stuart@>
2019-07-01 10:23:59 -0700

    import Header.jsx component into main APP.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
   return (
     <div>
-      <h1>Help Queue</h1>
+      <Header/>
       <h3>3a</h3>
       <h3>Thato and Haley</h3>
       <p><em>Firebase entries not saving!</em></p>
