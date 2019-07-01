3566af12189593fcff564445d585c2003990adcd
Reese Lee and Stuart McKay <reese@>
2019-07-01 11:03:42 -0700

    add Hot Module Replacement (HMR) and Embedded JS template for Index.html

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Ticket from "./Ticket";
 
 function TicketList(){
-
   return (
-    <Ticket/>
+    <Ticket
+      location="3A"
+      name="Thato and Haley"
+      issue="Firebase will not save record!"/>
   );
 }
 
