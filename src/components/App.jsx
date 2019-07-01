25fcb4927cd7a2d787eb530f60a21acaa91c2d63
Reese Lee and Stuart McKay <reese@>
2019-07-01 10:27:43 -0700

    add Ticket and TicketList components and adjust App.jsx code accordingly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(){
   return (
     <div>
       <Header/>
-      <h3>3a</h3>
-      <h3>Thato and Haley</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <TicketList/>
     </div>
   );
 }
